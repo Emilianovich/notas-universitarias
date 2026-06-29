5e17fcf7af3b627767a53b4e8c0bb865e5f4d95d
Emilianovich <emiolafagbemi02@gmail.com>
2026-06-28 19:27:49 -0500

    feat: created consistent success and error responses to client

--- apps/api/src/middlewares/general.ts
+++ apps/api/src/middlewares/general.ts
@@ -1,11 +1,23 @@
 import { createMiddleware } from "hono/factory"
-import type { Env } from "../index.js"
+import type {Env} from "../index.js"
 import { MongoService } from "../modules/db/MongoService.js"
+import {customLog} from "../services/logging/LogService.js";
+import {formatDate} from "@notas-universitarias/helpers";
 
 const generalMiddleware = createMiddleware<Env>(async (ctx, next) => {
+	const start = Date.now()
 	const mongoService = new MongoService()
 	await mongoService.connect()
 	ctx.set("mongoService", mongoService)
 	await next()
+	const end = Date.now()
+	if (ctx.res.status <= 299) {
+		ctx.res = ctx.json({
+			statusCode: ctx.res.status,
+			content: await ctx.res.json(),
+			issuedAt: formatDate(new Date()),
+		})
+	}
+	customLog({level: "info", message:`This request took ${end - start}ms`})
 })
 export default generalMiddleware
