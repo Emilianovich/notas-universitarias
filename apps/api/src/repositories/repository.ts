cfbf235bdab8adbbde4d0986efaaa384faa71854
Emilianovich <emiolafagbemi02@gmail.com>
2026-07-10 21:39:34 -0500

    refactor: changed init code for db and modified some db schema types

--- apps/api/src/repositories/repository.ts
+++ apps/api/src/repositories/repository.ts
@@ -1,12 +1,9 @@
-import {
-	type Collection,
-	type Document,
-} from "mongodb"
+import type { Collection, Document } from "mongodb"
 import type { MongoService } from "../modules/db/MongoService.js"
 
 export abstract class Repository<T extends Document> {
 	public readonly mongoService: MongoService
-	protected constructor(mongoService: MongoService) {
+	constructor(mongoService: MongoService) {
 		this.mongoService = mongoService
 	}
 	abstract getCollection(): Collection<T>
