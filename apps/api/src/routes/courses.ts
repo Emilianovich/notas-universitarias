002e0a746d92d53920748c40da474c2c75b08695
Emilianovich <emiolafagbemi02@gmail.com>
2026-08-12 14:57:52 -0500

    feat: added endpoint that returns previously registered courses

--- apps/api/src/routes/courses.ts
+++ apps/api/src/routes/courses.ts
@@ -1,7 +1,12 @@
 import { Hono } from "hono"
+import authMiddleware from "../middlewares/auth.js";
+import {getContextVars} from "../helpers/helpers.js";
+import {CourseService} from "../services/courses/CourseService.js";
 
 const coursesRoutes = new Hono().basePath("/courses")
-coursesRoutes.get("/", (ctx) => {
-	return ctx.text("Welcome to the courses route!")
+coursesRoutes.get("/", authMiddleware, async (ctx) => {
+	const { mongoService, userId } = getContextVars(ctx)
+	const courseService = new CourseService(mongoService)
+	return ctx.json(await courseService.getAvailableCoursesInAcademicPeriod(userId))
 })
 export default coursesRoutes
