ab986d5538a535d4ffec49c56cefb90e4116b5dc
Emilianovich <emiolafagbemi02@gmail.com>
2026-08-25 13:15:51 -0500

    fix: changed env.ts config

--- apps/api/src/modules/config/env.ts
+++ apps/api/src/modules/config/env.ts
@@ -9,12 +9,8 @@ const env = z
 				"El indicador de producción tiene que ser un booleano"
 			)
 			.transform((val) => val === "true"),
-		DB_URI_DEV: z
-			.string()
-			.min(37, "El URI de conexión a Mongo debe tener mínimo 37 caracteres"),
-		DB_URI_PROD: z
-			.string()
-			.min(37, "El URI de conexión a Mongo debe tener mínimo 37 caracteres"),
+		DB_URI_DEV: z.url().optional(),
+		DB_URI_PROD: z.url().optional(),
 		SESSION_COOKIE_NAME: z.string().default("user_session"),
 		CORS_ALLOWED_ORIGINS: z.string().default("http://localhost:3987"),
 		SESSION_COOKIE_SECURE: z
@@ -29,6 +25,15 @@ const env = z
 		),
 		TEMP_USER_COOKIE_NAME: z.string().default("temp-user")
 	})
+	.refine(
+		(data) =>
+			data.IS_PROD
+				? data.DB_URI_PROD !== undefined
+				: data.DB_URI_DEV !== undefined,
+		{
+			message: "Database URI for current environment is required"
+		}
+	)
 	.parse(process.env)
 
 export default env
